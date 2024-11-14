abc76085bfb24ab5be761b467795e6f7c7743716
zuzaszk <zu.aszkielowicz@gmail.com>
2024-11-14 14:49:00 +0100

    User endpoint implemented

--- src/main/java/com/pop/backend/config/WebConfig.java
+++ src/main/java/com/pop/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
