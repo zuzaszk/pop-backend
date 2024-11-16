ed8b73f609d719370576da739250738e4c1e1372
DasSchuechtern <bunny-pumpkin@hotmail.com>
2024-11-16 04:07:24 +0100

    make the main branch runable again

--- src/main/java/com/pop/backend/config/SecurityConfig.java
+++ src/main/java/com/pop/backend/config/SecurityConfig.java
@@ -1,47 +1,42 @@
 package com.pop.backend.config;
-
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.pop.backend.oauth.CustomOAuth2UserService;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        //for testing now, allow all requests
+                        .anyRequest().permitAll()
+                );
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
-        this.customOAuth2UserService = customOAuth2UserService;
+        return http.build();
     }
 
-    @Value("${frontend_url}")
-    private String frontendUrl;
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/", "/login").permitAll()
-                .anyRequest().authenticated())
-            .oauth2Login(oauth2 -> oauth2
-                .loginPage(frontendUrl + "/login")
-                .redirectionEndpoint(redirection -> redirection
-                    .baseUri("/login/oauth2/code/*"))
-                .userInfoEndpoint(userInfo -> userInfo
-                    .userService(customOAuth2UserService))
-                .defaultSuccessUrl(frontendUrl + "/#/dashboard", true)
-                )
-            .logout(logout -> logout
-                .logoutSuccessUrl(frontendUrl + "/login")
-                .permitAll());
-        return http.build();
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    // .allowedOrigins("http://localhost:5173")
+                    .allowedOrigins("*")
+                    //   .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    //   .allowCredentials(false);
+                    .allowCredentials(false);
+        }
+
+
     }
 
 }
--- src/main/java/com/pop/backend/config/WebConfig.java
+++ src/main/java/com/pop/backend/config/WebConfig.java
@@ -9,15 +9,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${frontend_url}")
-    private String frontendUrl;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(frontendUrl)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
